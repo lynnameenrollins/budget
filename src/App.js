23c151ded3ad4c4cf5db75a5e0ebd3584bb52ead
lynnameenrollins <lynnameenrollins@gmail.com>
2022-03-29 20:09:37 -0400

    starting budget assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import{useSelector, useDispatch} from 'react-redux';
 
 function App() {
+
+  require('react-dom');
+  window.React2 = require('react');
+  console.log("Checking to see if using on version of react", window.React1 === window.React2);
+  
+  let dispatch = useDispatch();
+  const budget = useSelector(state=>state.budget);
+  console.log("Budget: ", {budget})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Budget Planner</h1>
+      <div className="main-container">
+        {/* <h3 className= "budget">Budget: ${budget}  <button className = "updateBudget" type="button">Update</button></h3> */}
+        <h3 className= "remaining">Remaining: $Amount Remaining </h3>
+        <h3 className= "spent">Spent: $Amount Spent </h3>
+    </div>
     </div>
   );
 }
